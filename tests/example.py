0fb7f6a63ef9c356bb966c692bf332cc19fc134c
Anders Sildnes <andsild@gmail.com>
2014-02-10 15:38:35 +0100

    better example

--- tests/example.py
+++ tests/example.py
@@ -1,8 +1,22 @@
+#encoding:utf-8
+
 import unittest
 
-class Test(unittest.TestCase)
+class Test(unittest.TestCase):
+	def __init__(self):
+		super(Test, self).__init__();
+
 	def setUp(self):
+		self.myvar = "some value"
+
+	def tearDown(self):
+		""" Close everything that should be closed
+		"""
 		pass;
+	
+	def testTrue(self):
+		pass;
+
 
 if __name__== '__main__':
 	unittest.main();
