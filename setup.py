b249ed8655c7398caa1092f62500920093e0081a
Filip Egge <filip.egge@gmail.com>
2014-02-22 00:47:05 +0100

    Edited setyp.py to include requirements

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ from setuptools import setup
 import os
 
 # Put here required packages
-packages = ['Django<=1.6',]
+packages = ['Django<=1.6','django-grappelli','django-filebrowser','django-registration', 'south',]
 
 if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
      packages.append('django-redis-cache')
      packages.append('hiredis')
 
-setup(name='YourAppName',
+
+setup(name='GentleIDI',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
+      author='GentleCoding',
       author_email='example@example.com',
       url='https://pypi.python.org/pypi',
       install_requires=packages,
--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -60,7 +60,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    #'debug_toolbar',
     'contest',
     'article',
 	'django_jenkins',
