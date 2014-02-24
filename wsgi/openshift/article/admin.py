deb3a3b83ce97b9336cbf48d6cb3ffdb0a74f494
konradaa <haakon.konrad@gmail.com>
2014-02-24 13:38:46 +0100

    Prøvd å fikse dato og author

--- wsgi/openshift/article/admin.py
+++ wsgi/openshift/article/admin.py
@@ -4,11 +4,20 @@ from openshift.article.models import Article
 
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
+    
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user()
+        obj.save()
+    
     class Media:
         # Include javascript for wysiwyg editor
         js = [
             '/static/grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js',
             '/static/openshift/js/tinymce_setup.js',
         ]
+    
+        
+
+
 # Add article to admin page
 admin.site.register(Article, ArticleAdmin)
