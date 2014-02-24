f2dbe525ab3ed13a4f1da2391b7bf27939634bd5
konradaa <haakon.konrad@gmail.com>
2014-02-24 13:38:46 +0100

    Prøvd å fikse dato og author

--- wsgi/openshift/article/models.py
+++ wsgi/openshift/article/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User 
+
 
 # Create your models here.
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add = True)
     contest = models.ForeignKey('contest.Contest')
     text = models.TextField()
+    author = models.ForeignKey(User, null = True, blank = True)
+    
     
     def __str__(self):      #Default return string
         return self.title
