b017e64ab394efd7e23dbb09671454feccdf6d9d
konradaa <haakon.konrad@gmail.com>
2014-02-26 12:44:31 +0100

    working news feed. With correct atuhor and date

--- wsgi/openshift/contest/views.py
+++ wsgi/openshift/contest/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from openshift.contest.models import Contest
+from openshift.article.models import Article
 # Create your views here.
 
 def index(request):
-    
-    return render(request, 'base.html')
+    url = request.path.split('/')[1]
+    article_list = Article.objects.all().filter(contest__url = url).order_by("-created_at")
+    context = {'article_list' : article_list, 
+               }    
+    return render(request, 'contest/index.html', context)
+
