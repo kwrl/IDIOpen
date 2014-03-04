0352f0c3f80573fb9b602af1287761fdd0fb8f11
Anders Sildnes <andsild@gmail.com>
2014-03-04 22:11:38 +0100

    only see contests that are published. some code refactoring

--- wsgi/openshift/contest/views.py
+++ wsgi/openshift/contest/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404;
+
+import datetime;
+
+from contest.models import Contest;
 from article.models import Article
-# Create your views here.
 
 
-def index(request):
-    url = request.path.split('/')[1]
-    article_list = Article.objects.all().filter(contest__url = url).order_by("-created_at")
-    context = {'article_list' : article_list, 
-               }    
+import ipdb;
+
+def index(request, contestURL):
+    contest =  get_object_or_404( 
+                    Contest.objects.exclude(
+                        publish_date__gt=datetime.datetime.now()) \
+                    .filter(
+                        url=contestURL));
+
+    article_list = Article.objects.all().filter(contest__url = contest.url) \
+                                        .order_by("-created_at")
+    context = {'article_list' : article_list, }    
+
     return render(request, 'contest/index.html', context)
 
