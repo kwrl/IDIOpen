c661409d882525cfc134f08a316044c17d1db29f
konradaa <haakon.konrad@gmail.com>
2014-03-06 12:22:40 +0100

    The system now knows if your invites are registered user or not. It will then send an email, depinding og this.

--- wsgi/openshift/contest/forms.py
+++ wsgi/openshift/contest/forms.py
@@ -7,6 +7,10 @@ Created on Feb 12, 2014
 @author: tino, haakon 
 '''
 
+
+'''
+TODO: add support ofor leader. We need the log in to do that.
+'''
 class Team_Form(forms.ModelForm):
     email_one = forms.EmailField();
     email_two = forms.EmailField();
