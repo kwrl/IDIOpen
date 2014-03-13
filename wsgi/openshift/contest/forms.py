46264042e70257139d82b9ebfe383a1b5c3ca5f5
Tino92 <tinolazreg@gmail.com>
2014-03-13 20:40:07 +0100

    Adds error message to editteam and separated edit and delete button, fixed bootstrap layout

--- wsgi/openshift/contest/forms.py
+++ wsgi/openshift/contest/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from models import Team
+from django.forms.widgets import CheckboxSelectMultiple
+from django.forms.models import ModelMultipleChoiceField
+from userregistration.models import CustomUser
 
 '''
 Created on Feb 12, 2014
@@ -19,5 +22,22 @@ class Team_Edit(forms.ModelForm):
 class Team_Form(Team_Edit):
     email_one = forms.EmailField(required=False);
     email_two = forms.EmailField(required=False);
-      
     
+class CustomSelectMultiple(ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return "%s:" %(obj.email)
+    
+class Team_Delete_Members(forms.ModelForm):
+    #  members = ModelMultipleChoiceField(widget=CheckboxSelectMultiple(), required=True)
+  
+    def __init__(self,*args,**kwargs):
+        super(Team_Delete_Members, self).__init__(*args,**kwargs)
+        #qs = Team.objects.get(pk = self.instance.pk)
+        self.fields['members'] = ModelMultipleChoiceField(
+            # TODO exlude leader from qs
+            queryset= CustomUser.objects.filter(members__in=Team.objects.filter(pk = self.instance.pk)), 
+                                       widget=CheckboxSelectMultiple())
+    class Meta:
+        model = Team
+        fields = ['members']
+    #    widgets = {'members':CheckboxSelectMultiple(),}
