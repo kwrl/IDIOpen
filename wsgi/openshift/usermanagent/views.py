407f9bab676807c1742a436e0d586a28a3a5539c
Anders Sildnes <andesil@localhost.localdomain>
2014-03-14 22:10:46 +0100

    cleanup, removed unused files

--- wsgi/openshift/usermanagent/views.py
+++
@@ -1,5 +0,0 @@
-from django.contib.auth.decorators import login_required;
-from userregistration import models as reg_models;
-#from usermanagent import models as man_models;
-from contest import models as con_models;
-from django.shortcuts import render;
