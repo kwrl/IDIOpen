84225ad828b2d868a7bac9424ea3846e7064130d
Tino92 <tinolazreg@gmail.com>
2014-03-16 16:33:08 +0100

    adds leave team functionality for leader and members

--- wsgi/openshift/contest/urls.py
+++ wsgi/openshift/contest/urls.py
@@ -18,5 +18,6 @@ urlpatterns = patterns('',
     url(r'^registration/registrationComplete/$', views.registrationComplete, name = 'register_team_complete'),
     url(r'^team$', views.teamProfil, name = 'team_profile'),
     url(r'^editteam$', views.editTeamProfil, name = 'team_edit'),
+     url(r'^leaveteam$', views.leave_team, name = 'team_leave'),
     
 )
