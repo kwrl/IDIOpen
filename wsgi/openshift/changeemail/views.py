a2a4f322aea8f8cd9e9a12e39c8659a834826d93
Anders Sildnes <andsild@gmail.com>
2014-03-19 22:16:44 +0100

    changing email works, but it's not implemented; need to update other fields, etc

--- wsgi/openshift/changeemail/views.py
+++ wsgi/openshift/changeemail/views.py
@@ -6,29 +6,9 @@ from .models import ChangeEmail;
 
 class ChangeEmailView(TemplateView):
     def get(self, request,  *args, **kwargs):
-        '''
-        TODO: Swap email
-        '''
-        
-        #activated_user = self.activate(request, *args, **kwargs)
-        #activated_user = ChangeEmail(request.user, "penis").activate_email(*args);
-        activated_user = ChangeEmail.objects.get_instance(kwargs['activation_key']);
-        activated_user.new_email = "poop";
-        activated_user.save();
+        instance = ChangeEmail.objects.get_instance(kwargs['activation_key']);
+        instance.updateUser();
+        instance.delete();
 
         return render(request);
-        
-    def activate(self, request, activation_key):
-        """
-        Given an an activation key, look up and activate the user
-        account corresponding to that key (if possible).
-
-        After successful activation, the signal
-        ``registration.signals.user_activated`` will be sent, with the
-        newly activated ``User`` as the keyword argument ``user`` and
-        the class of this backend as the sender.
-        """
-        
-        return activated_user
-        
 # EOF
