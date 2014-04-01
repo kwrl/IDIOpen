4e1192cc197b9988cfd8fcd9b7c46e9cb5da50c0
Tino92 <tinolazreg@gmail.com>
2014-04-01 15:49:55 +0200

    Problem description template that renders the problem, and allows a contestant to upload a submission

--- wsgi/openshift/teamsubmission/views.py
+++ wsgi/openshift/teamsubmission/views.py
@@ -1,28 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contest.views import get_current_contest
 from contest.models import Team
 from execution.models import Problem
+
 from .models import Submission
-from itertools import groupby, imap
+from .forms import SubmissionForm
+
+from itertools import groupby, imap, izip_longest
 from operator import itemgetter
+from django.contrib import messages
 
 def submission_problem(request, problemID):
     #TODO: maybe a nicer url than numeric ID
+    
+    problem = get_object_or_404(Problem.objects.filter(pk=problemID))
     user = request.user
-    problem = Problem.objects.filter(pk=problemID)
     con = get_current_contest(request)
-    team = Team.objects.filter(contest=con).filter(members__id = user.id)
-
-    submission = Submission.objects.filter(team=team).order_by('date_uploaded')
-    if len(submission) > 0:
+    team = Team.objects.filter(contest=con).get(members__id = user.id)
+    
+    submission = Submission.objects.filter(team=team).filter(problem=problemID).order_by('date_uploaded')
+            
+    if len(submission.values_list()) > 0:
         submission = submission[0]
+        problem = submission.problem
     else:
-        submission = None
-        
+        submission = Submission()
+        submission.problem = problem
+        submission.team = team
+    
+    if request.method == "POST":
+        form = SubmissionForm(request.POST, request.FILES,
+                               instance=submission)
+        if form.is_valid():
+            form.save()
+            
+    form = SubmissionForm(instance=submission);  
     
     context = {
              'problem' : problem,
              'submission' : submission,
+             'submission_form' : form,
               }
     
     return render(request,
@@ -38,15 +55,15 @@ def submission_view(request):
     
     team = Team.objects.filter(contest=con).filter(members__id = user.id)
     problems = Problem.objects.filter(contest=con)
-    
     submissions = Submission.objects.filter(team=team).order_by('date_uploaded')
-    # Get only on submission per problem. 
+    # Get only one submission per problem. 
     # The submission is the first one returned, as per date_uploaded
     ret_submissions = map(next, imap(itemgetter(1),
-                                groupby(submissions, lambda x:x.problem)))
-   
+                          groupby(submissions, lambda x:x.problem)))
+        
     listProbSub = [SubJoinProb(sub, prob) 
-                   for (sub, prob) in zip(ret_submissions, problems)]
+                   for (sub, prob) in izip_longest(ret_submissions, problems)]
+    
     
     context = {
                'problems' : problems,
@@ -57,11 +74,12 @@ def submission_view(request):
 
 class SubJoinProb(object):
     def __init__(self, submission, problem):
-        self.submission = submission
-        self.submission.submission = \
-            str(submission.submission).split('/')[-1]
-        self.submission.date_uploaded = \
-                submission.date_uploaded.strftime('%H:%M:%S')
+        if submission is not None:
+                self.submission = submission
+                self.submission.submission = \
+                    str(submission.submission).split('/')[-1]
+                self.submission.date_uploaded = \
+                        submission.date_uploaded.strftime('%H:%M:%S')
         self.problem = problem 
         
 # EOF
