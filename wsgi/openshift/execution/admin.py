371c0d22a2e82db5705bfd619014f4837e59a823
Tino92 <tinolazreg@gmail.com>
2014-04-04 13:47:39 +0200

    You don't have to manually set author when creating a Problem

--- wsgi/openshift/execution/admin.py
+++ wsgi/openshift/execution/admin.py
@@ -11,6 +11,13 @@ class TestCaseAdmin(admin.ModelAdmin):
 admin.site.register(TestCase, TestCaseAdmin)
 
 class ProblemAdmin(admin.ModelAdmin):
-    ordering = ('title', 'description', 'textFile')
-    
+    list_display = ('title', 'author', 'contest') 
+    fields = ('title', 'description', 'textFile', 'contest')
+    # Set author, to the user/admin who created the article
+    def save_model(self, request, obj, form, change):
+        # Only update author if author = None
+        if getattr(obj, 'author', None) is None:
+            obj.author = request.user
+        obj.save()        
+        
 admin.site.register(Problem, ProblemAdmin)
