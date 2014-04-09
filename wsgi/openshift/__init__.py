2dd65fd492e7c22035201f4f32d287f1fd734398
Filip Egge <filip.egge@gmail.com>
2014-04-09 13:20:24 +0200

    Started celery integration

---
+++ wsgi/openshift/__init__.py
@@ -0,0 +1,10 @@
+'''
+Created on Apr 9, 2014
+
+@author: filip
+'''
+from __future__ import absolute_import
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app
