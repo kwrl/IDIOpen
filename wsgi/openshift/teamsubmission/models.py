02556011287486f97b6fa6adc30ecabe9822f02e
Eirik Fosse <eirik92fosse@gmail.com>
2014-04-11 11:52:20 +0200

    Added position field in highscore statistics

--- wsgi/openshift/teamsubmission/models.py
+++ wsgi/openshift/teamsubmission/models.py
@@ -9,14 +9,17 @@ from execution.models import Problem
 from contest.models import Team, Contest
 from django.core.files.storage import FileSystemStorage
 
+import operator
+
 private_media = FileSystemStorage(location=settings.PRIVATE_MEDIA_ROOT,
                                   base_url=settings.PRIVATE_MEDIA_URL,
                                   )
 
 def get_upload_path(instance, filename):
-    """ Dynamically decide where to upload the case,
-        based on the foreign key in instance, which is required to be 
-        a Submission.
+    """ 
+    Dynamically decide where to upload the case,
+    based on the foreign key in instance, which is required to be 
+    a Submission.
     """
     # path.join appends a trailing / in between each argument
     return os.path.join("%s" % 'somewhere',
@@ -25,18 +28,20 @@ def get_upload_path(instance, filename):
 
 class ScoreManager(models.Manager):
     def get_problem_score(self, team, problem, contest):
-        """ This constant is the penalty
-            for delivering incorrect submissions.
+        """ 
+        This constant is the penalty
+        for delivering incorrect submissions.
         """
         submission_penalty = contest.penalty_constant
         submissions = Submission.objects.filter(team=team).filter(problem=problem).filter(solved_problem=False).order_by('-date_uploaded')
         correctSubmissions = Submission.objects.filter(team=team).filter(problem=problem).filter(solved_problem=True).order_by('date_uploaded')
         
-        """ The statistics are:
-            [total score,
-            time score,
-            submission score,
-            number of submissions]
+        """ 
+        The statistics are:
+        [total score,
+        time score,
+        submission score,
+        number of submissions]
         """
         statistics = [0, 0, 0, len(submissions)]
         if(len(correctSubmissions) > 0):
@@ -53,20 +58,21 @@ class ScoreManager(models.Manager):
     def get_team_score(self, team, contest):
         problems = Problem.objects.filter(contest=contest)
         
-        """ The statistics are:
-            [total score,
-            solved problems,
-            time score,
-            problem 1 submissions,
-            ...
-            problem n submissions,]
+        """ 
+        The statistics are:
+        [solved problems,
+        total score,
+        time score,
+        problem 1 submissions,
+        ...
+        problem n submissions,]
         """
         statistics = [0, 0, 0]
         for problem in problems:
             problemStat = ScoreManager.get_problem_score(self, team, problem, contest)
-            statistics[0] = statistics[0] + problemStat[0]
             if problemStat[0]:
-                statistics[1] = statistics[1] + 1
+                statistics[0] = statistics[0] + 1
+            statistics[1] = statistics[1] + problemStat[0]
             statistics[2] = statistics[2] + problemStat[1]
             statistics.append(problemStat[3])
         return statistics
@@ -74,35 +80,44 @@ class ScoreManager(models.Manager):
     def get_highscore(self, contest):
         teams = Team.objects.filter(contest=contest)
         
-        """ The statistics are:
-            [team name,
-            total score,
-            solved problems,
-            time score,
-            problem 1 submissions,
-            ...
-            problem n submissions,]
+        """ 
+        The statistics are:
+        [position,
+        team name,
+        solved problems,
+        total score,
+        time score,
+        problem 1 submissions,
+        ...
+        problem n submissions,]
         """
         statistics = []
         
-        """ zeros is a list of teams that have 0 in total score. These teams haven't
-            solved any problems, and should beat the bottom of the scoreboard.
+        """ 
+        zeros is a list of teams that have 0 in total score. These teams haven't
+        solved any problems, and should be at the bottom of the scoreboard.
         """
         zeros = []
         for team in teams:
             teamStats = ScoreManager.get_team_score(self, team, contest)
-            highscore = [team.name]
+            highscore = [0, team.name]
             if(teamStats[0]):
                 for field in teamStats:
                     highscore.append(field)
                 statistics.append(highscore)
             else:
+                teamStats[0] = ""
                 for field in teamStats:
                     highscore.append(field)
                 zeros.append(highscore)
-        sorted(statistics, key=lambda score: score[1])
+        statistics = sorted(statistics, key=lambda x : (-x[2], x[3]))
+        for i in range(len(statistics)):
+            statistics[i][0] = i + 1
+        
         for s in zeros:
+            s[0] = len(statistics) + 1
             statistics.append(s)
+        print(statistics)
         return statistics
 
 class Submission(models.Model):
