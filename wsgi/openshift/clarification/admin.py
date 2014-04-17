b539a88d0c4d6f941628532f334b70f9afb48464
Tino92 <tinolazreg@gmail.com>
2014-04-17 23:18:16 +0200

    MessageAnswer model, connected to Message model ,and available in admin interface, commented out code not yet fixed

--- wsgi/openshift/clarification/admin.py
+++ wsgi/openshift/clarification/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from .models import Message
+from .models import MessageAnswer, Message
 # Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model = MessageAnswer
+
 class MessageAdmin(admin.ModelAdmin):
+    inlines = [CommentInline,]
     list_display = ('subject', 'sender', 'sent_at', 'answared_by', 'answared_at')
     list_filter = ('sent_at',)
     date_hierarchy = 'sent_at'
     search_fields = ('sender',)
-    change_form_template = 'index.html'
+    ordering = ('-sent_at',)
     
+
 admin.site.register(Message, MessageAdmin)
 
 
