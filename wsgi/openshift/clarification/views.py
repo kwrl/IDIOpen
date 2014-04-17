8693ecbef4ea2cd8aec359ad8791d95f83bad7da
Konrad <typo@Typo-pc>
2014-04-17 17:46:50 +0200

    Added clarification design

--- wsgi/openshift/clarification/views.py
+++ wsgi/openshift/clarification/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .forms import MessageForm
 
 # Create your views here.
+
+def clarification(request):
+    
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+    else:
+        form = MessageForm()
+    
+    context = {
+               'clarification_form':form
+               }
+     
+    return render(request, 'clarification.html', context)
+    
+    
+    
--- wsgi/openshift/teamsubmission/urls.py
+++ wsgi/openshift/teamsubmission/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from .views import submission_view, submission_problem, highscore_view
+from clarification.views import clarification
 
 urlpatterns = patterns('',
     url(r'^$', submission_view, name = 'submission_page'),
     url(r'(?P<problemID>[\d]+)', submission_problem),
     url(r'highscore', highscore_view),
+    url(r'clarification', clarification),
 )
 
 # EOF
