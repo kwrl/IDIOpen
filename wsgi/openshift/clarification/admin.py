319e13f5f115893a6cc4814889a3df40a59f1393
Tino92 <tinolazreg@gmail.com>
2014-04-17 21:14:01 +0200

    a signal is sent when a message is created

--- wsgi/openshift/clarification/admin.py
+++ wsgi/openshift/clarification/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Message
 # Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'sender', 'sent_at', 'answered_by', 'answered_at')
+    list_filter = ('sent_at',)
+    date_hierarchy = 'sent_at'
+    search_fields = ('sender',)
+    change_form_template = 'index.html'
+    
+admin.site.register(Message, MessageAdmin)
+
+
+
