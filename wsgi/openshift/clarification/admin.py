72bcc920c0d9d2323213dbc29e7e638d1abe3c9c
Tino92 <tinolazreg@gmail.com>
2014-04-18 12:23:12 +0200

    deleted migrations, before I pull new migrations to avoid conflicts

--- wsgi/openshift/clarification/admin.py
+++ wsgi/openshift/clarification/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from .models import MessageAnswer, Message
 # Register your models here.
 
-class CommentInline(admin.TabularInline):
+class AnswerInline(admin.TabularInline):
     model = MessageAnswer
+    # Don't show any extra MessageAnswer's on default
+    extra = 0
 
 class MessageAdmin(admin.ModelAdmin):
-    inlines = [CommentInline,]
+    inlines = (AnswerInline,)
     list_display = ('subject', 'sender', 'sent_at', 'answared_by', 'answared_at')
     list_filter = ('sent_at',)
     date_hierarchy = 'sent_at'
