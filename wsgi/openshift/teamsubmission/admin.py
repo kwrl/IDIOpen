833c66f9f6b0d67c6c963d59b9e32e5e66275a96
= <haakongk@stud.ntnu.no>
2014-04-19 11:13:39 +0000

    Execution log entry added

--- wsgi/openshift/teamsubmission/admin.py
+++ wsgi/openshift/teamsubmission/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Submission
+from .models import Submission, ExecutionLogEntry
 
 class SubmissionAdmin(admin.ModelAdmin):
     list_display = ('date_uploaded', 'text_feedback', 'team', 'problem',
                     'solved_problem', 'submission')
 
 admin.site.register(Submission, SubmissionAdmin)
+admin.site.register(ExecutionLogEntry)
 
 # EOF
