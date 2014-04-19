e71bfd72df82ceb447d21d6b8535e46cd2ec9a11
Eirik Fosse <eirik92fosse@gmail.com>
2014-04-19 15:53:38 +0200

    merge fix

--- wsgi/openshift/contest/urls.py
+++ wsgi/openshift/contest/urls.py
@@ -4,9 +4,9 @@ Created on Feb 12, 2014
 @author: filip
 '''
 from django.conf.urls import patterns, include, url
-from contest import views
-from article import views as articleview
-from teamsubmission.views import highscore_view
+from openshift.contest import views
+from openshift.article import views as articleview
+from openshift.teamsubmission.views import highscore_view
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', views.index, name='contest_list'),
