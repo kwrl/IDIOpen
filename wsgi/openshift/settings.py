aa7f56ce215570d5859f2d5b41e775c84c85a265
Filip Egge <filip.egge@gmail.com>
2014-04-20 20:01:05 +0200

    Minor changes to highscore ajax

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -60,7 +60,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    #'debug_toolbar',
     'openshift.contest',
     'openshift.article',
     'openshift.userregistration',
