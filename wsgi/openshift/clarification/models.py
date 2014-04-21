0df0708b261efe513f1debff984b0dd135421825
typo <typo@Typo-pc>
2014-04-21 18:05:08 +0200

    New clarification help text

--- wsgi/openshift/clarification/models.py
+++ wsgi/openshift/clarification/models.py
@@ -8,9 +8,9 @@ class Question (models.Model):
         verbose_name = "or add an answer to a question"
         verbose_name_plural = "View and answer questions"
         
-    subject     = models.CharField(max_length = 120)
-    body        = models.TextField(max_length = 355)
-    sender      = models.ForeignKey(Team)    
+    subject     = models.CharField(max_length = 120, help_text = "Max 120 chars")
+    body        = models.TextField(max_length = 355, help_text = "Max 355 chars")
+    sender      = models.ForeignKey(Team)
     sent_at     = models.DateTimeField(null=True, blank=True, auto_now = True)
     contest     = models.ForeignKey(Contest)
     answered    = models.BooleanField(default = False, 
@@ -26,9 +26,9 @@ class QuestionAnswer(models.Model):
         verbose_name = "Clarification"
         verbose_name_plural = "View/add Clarifications"
    
-    subject     = models.CharField(max_length = 120)
-    body        = models.TextField(max_length = 355)
+    subject     = models.CharField(max_length = 120, help_text = "Max 120 chars")
+    body        = models.TextField(max_length = 355, help_text = "Max 355 chars")
     answered_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank = True, null=True) 
     answered_at = models.DateTimeField(null=True, blank=True, auto_now = True)  
-    question    = models.ForeignKey(Question, null=True, blank=True, default = None,  help_text = "?")
-    contest     = models.ForeignKey(Contest)
+    question    = models.ForeignKey(Question, null=True, blank=True, default = None)
+    contest     = models.ForeignKey(Contest, help_text = "Please specify which contest to show this clarification")
