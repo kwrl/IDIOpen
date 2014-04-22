30b81095fd3ee7b550dcb3fe0f1573036ad5c91a
typo <typo@Typo-pc>
2014-04-22 23:03:46 +0200

    Ajax added to balloon system.

--- wsgi/openshift/balloon/admin.py
+++ wsgi/openshift/balloon/admin.py
@@ -31,7 +31,12 @@ def _get_table_lists():
     balloons = BalloonStatus.objects.all()
     balloon_subs = dict([(ball.submission.pk, ball.timestamp) for ball in balloons])
     corrrect_submissions = Submission.objects.filter(team__onsite = 'True').filter(solved_problem = 'True')
-    
+
+    if corrrect_submissions:
+        last_solved_submission = corrrect_submissions.latest('date_uploaded')
+    else:
+        last_solved_submission = None
+        
     given_balloon, not_given_balloon = [], []
 
     for sub in corrrect_submissions:
@@ -41,21 +46,25 @@ def _get_table_lists():
         else:
             not_given_balloon.append(BalloonView(
                                 sub, timestamp=sub.date_uploaded))
-    return given_balloon, not_given_balloon
+            
+    return given_balloon, not_given_balloon, last_solved_submission
 
 def balloon_home(request):
-
     form = None
     if request.method == 'POST':
         form = BalloonSubmissionForm(request.POST);
 
         if form.is_valid():
             form.save()
+    given_balloon, not_given_balloon, last_solved_submission = _get_table_lists()
+    
+    if last_solved_submission:
+        last_solved_submission = last_solved_submission.pk
 
-    given_balloon, not_given_balloon = _get_table_lists()
     context = {
                'given_balloon': given_balloon,
                'not_given_balloon': not_given_balloon,
+               'last_solved_submission' :  last_solved_submission,
                }
 
     return render(request,
