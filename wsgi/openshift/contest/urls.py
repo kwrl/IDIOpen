89f22aa987c377c3202d28cd6481d1e48c40ee7f
Eirik Fosse <eirik92fosse@gmail.com>
2014-04-23 20:43:58 +0200

    removed one of the highscore views(redundant), fixed Judge_supervisor bug

--- wsgi/openshift/contest/urls.py
+++ wsgi/openshift/contest/urls.py
@@ -6,7 +6,7 @@ Created on Feb 12, 2014
 from django.conf.urls import patterns, include, url
 from openshift.contest import views
 from openshift.article import views as articleview
-from openshift.teamsubmission.views import highscore_view, highscore_view_res
+from openshift.teamsubmission.views import highscore_view
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', views.index, name='contest_list'),
@@ -28,7 +28,7 @@ urlpatterns = patterns('',
     url(r'^pages/(?P<article_url>[^/]+)/$', articleview.detail_url, name='article_detail_url'), 
     url(r'^cage$', views.cage_me, name = 'nic_cage'), 
     url(r'^highscore/$', highscore_view, name = 'highscorePage'),
-    url(r'^highscore/(?P<sort_res>\w+)/$', highscore_view_res, name = 'highscorePageRes'),
+    url(r'^highscore/(?P<sort_res>\w+)/$', highscore_view, name = 'highscorePageRes'),
 )
 
 # EOF
