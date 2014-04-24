3a2782606dd5efedf986c9e67307e64e51737564
Anders Sildnes <andesil@dhcp-062223.wlan.ntnu.no>
2014-04-24 13:53:06 +0200

    loadtesting seems to work for submissions. You can now also register users + team

--- loadtest/locustfile.py
+++ loadtest/locustfile.py
@@ -1,19 +1,26 @@
+""" You need trailing slashes in all URLS
+"""
+
 from locust import HttpLocust, TaskSet, task
 
 from random import randint
 
 files = [open('test.java', 'r'), open('test2.java', 'r')]
 usernames = open('emails.txt', 'r').read()
-usernames = usernames.split('\n')[::-1]
+usernames = usernames.split('\n')
 usernames.pop()
+
+problems = [1, 6, 7, 8, 11, 12, 13, 14, 17, 18, 19, 21, 22]
 
 class NestTask(TaskSet):
     @task
     class SubmissionTask(TaskSet):
         def on_start(self):
+            global usernames
+            user = usernames[randint(0, len(usernames)-1)]
             loginDict = {
-                    'username': 'test@test.no',
-                    'password': 'test123',
+                    'username': user,
+                    'password': 'password',
                     }
 
             c = self.client.post("/open14/accounts/login/", loginDict)
@@ -29,17 +36,56 @@ class NestTask(TaskSet):
                         'submission' : file_upload,
                         }
 
-            c = self.client.post("/open14/problem/1/", submission_json,
+            problem = problems[randint(0, len(problems)-1)]
+            c = self.client.post("/open14/problem/" + str(problem) + "/", submission_json,
                                  files=file_json)
             print c.content
 
-    @task
-    class RegistrationTask(TaskSet):
-        @task
-        def register(self):
+    #@task
+    class RegisterTeam(TaskSet):
+        global usernames
+        #user = usernames[randint(0, len(usernames)-1)]
+        user = usernames.pop()
+
+        def on_start(self):
             global usernames
+            user = usernames[randint(0, len(usernames)-1)]
+            #user = self.user
+            loginDict = {
+                    'username': user,
+                    'password': 'password',
+                    }
+
+            c = self.client.post("/open14/accounts/login/", loginDict)
+
+        @task
+        def registerTeam(self):
+            """
+            """
+            # Strip away ".com" and remove the at and period mark
+            #teamname = "team" + self.user.translate(None, '@.')[:-3]
+            teamname = "team" + self.user.translate(None, '@.')[:-3]
+            #teamname = "team" + usernames[randint(0, len(usernames)-1)]
+            teamurl = "/open14/team/register/"
             postDict = {
-                    'email'       : usernames.pop(),
+                    'name'       : teamname,
+                    'onsite'     : 'True',
+                    'member_two' : '',
+                    'member_one' : '',
+                }
+
+            c = self.client.post(teamurl, postDict)
+            self.interrupt(reschedule=True)
+
+
+    #@task
+    class RegisterUser(TaskSet):
+        global usernames
+        user = usernames[randint(0, len(usernames)-1)]
+        #@task
+        def registerUser(self):
+            postDict = {
+                    'email'       : self.user,
                     'first_name'  : 'first',
                     'last_name'   : 'last',
                     'nickname'    : 'nick',
@@ -54,6 +100,7 @@ class NestTask(TaskSet):
             #print c
             print c
 
+        
 class WebsiteUser(HttpLocust):
     """ Emulate a real user browsing the pages.
     """
@@ -61,7 +108,7 @@ class WebsiteUser(HttpLocust):
     #task_set = RegistrationTask
     task_set = NestTask
     """ minimum wait before doing a task as a user """
-    min_wait = 5000 #
+    min_wait = 10000 #
     """ maximum wait before executing a task"""
     max_wait = 15000 #
     """ Weight, how often to run task relative to others"""
--- loadtest/run.sh
+++ loadtest/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-locust --no-web -c 100 -r 100 -L CRITICAL
+while :; do locust --no-web -c 1 -r 1 -L CRITICAL ; done
+#locust --no-web -c 100 -r 100 -L INFO -r2
+#locust --no-web -c 100 -r 100 -L INFO -r2
 
 # EOF
