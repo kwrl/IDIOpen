ddb384c5cf7990231c69f3df7886a4a743d436ee
Eirik Fosse <eirik92fosse@gmail.com>
2014-04-24 14:37:01 +0200

    fixed bug with unicode characters in offsite name in highscore widget

--- wsgi/openshift/teamsubmission/ajax.py
+++ wsgi/openshift/teamsubmission/ajax.py
@@ -97,10 +97,10 @@ def build_html_table(stats):
         #Onsite/ofsite
         if stats[s][2]:
             #string += "<td>" + "<span class=\"label label-success\"> \" \" </span>"  + "</td>"
-            if len(stats[s][2]) > 3: 
-                string += "<td>" + stats[s][2][:3] + ".." "</td>"
+            if len(unicode(stats[s][2])) > 4:
+                string += "<td>" + unicode(stats[s][2])[:4] + ".." "</td>"
             else:
-                string += "<td>" + stats[s][2][:3] + "</td>"
+                string += "<td>" + stats[s][2] + "</td>"
         else: 
             string += "<td>" + "Yes"  + "</td>"
         
