42a6c9ea91644690f29125c92fa752d0d2301fca
Anders Sildnes <andsild@gmail.com>
2014-04-24 14:17:53 +0000

    fixed error, was not uploading files

--- loadtest/run.sh
+++ loadtest/run.sh
@@ -2,7 +2,8 @@
 
 while :; 
 do
-    locust --no-web -c 1 -r 1 -L CRITICAL & PROC=$1; sleep 0.7; kill $!
+    locust --no-web -c 1 -r 1 -L INFO  &
+    PROC=$1; sleep 0.7; kill $!
 done
 #locust --no-web -c 100 -r 100 -L INFO -r2
 #locust --no-web -c 100 -r 100 -L INFO -r2
