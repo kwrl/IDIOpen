00e9ecfb8b10068f9b835412eb8a8546a7b4f29c
Anders Sildnes <andsild@gmail.com>
2014-04-27 16:15:45 +0000

    refactored code

--- mysql/fetch_data.py
+++ mysql/fetch_data.py
@@ -3,6 +3,11 @@ from collections import defaultdict
 from subprocess import Popen
 from shlex import split
 
+from os import path, makedirs
+import re, string; 
+
+from zipfile import ZipFile
+
 # local import
 from latex_file import CONTESTANT_PARSELINE, TEAM_PARSELINE, LATEX_PARSE
 
@@ -15,7 +20,8 @@ ORDER BY T.id
 ;
 """
 
-MYSQL_CON = mdb.connect('localhost', 'root', '12345', 'gentleidi');
+#MYSQL_CON = mdb.connect('hv-6146.idi.ntnu.no', 'gentle', 'tacosushi', 'gentleidi', 3306);
+#MYSQL_CON = mdb.connect('129.241.106.146', 'gentle', 'tacosushi', 'gentleidi', 3306);
 
 def renderingStr(contestants):
     con_prefix = r"""\textbf{"""
@@ -43,16 +49,33 @@ def get_team_contestant_dict():
     return team_members_dict
 
 def process_team_contestants():
-    for team_name, contestants in get_team_contestant_dict().iteritems():
+    team_contestant_dict = get_team_contestant_dict()
+    yesno = raw_input("This will make latex for each %s teams - are you sure?"
+                     % (len(team_contestant_dict)) +  "(Y/N)\n" )
+    if yesno is not 'Y':
+        return
+
+    dir_dest = "/tmp/teams/"
+    if not path.exists(dir_dest):
+        makedirs(dir_dest)
+
+    pattern = re.compile('[\W_]+')
+
+    i = 0
+    for team_name, contestants in team_contestant_dict.iteritems():
         tex_dict = {
                betweenParanthesis(CONTESTANT_PARSELINE) : renderingStr(contestants),
                betweenParanthesis(TEAM_PARSELINE)       : team_name}
 
-        with open('cover.tex','w') as f:
+        file_name = dir_dest + pattern.sub('', team_name) +  str(i) + '.tex'
+    
+        i += 1
+
+        with open(file_name,'w') as f:
             f.write(LATEX_PARSE%tex_dict)
 
-    proc=Popen(split('pdflatex cover.tex'))
-    proc.communicate()
+        proc=Popen(split('pdflatex -output-directory %s ' % (dir_dest) + file_name))
+        proc.communicate()
 
 def extract_to_csv():
     #INTO OUTFILE '/tmp/out", DATE_FORMAT(now(),'%d-%h%s'), ".csv' 
@@ -60,7 +83,7 @@ def extract_to_csv():
     SQL_TO_CSV = """
 SELECT @sql_cmd := concat("
 SELECT T.name, U.email 
-    INTO OUTFILE '/tmp/FILE", DATE_FORMAT(now(),'%d-%h%s'), ".csv' 
+    INTO OUTFILE '/tmp/csv_teamuser", DATE_FORMAT(now(),'%d-%h%s'), ".csv' 
     FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
 FROM contest_team AS T , userregistration_customuser AS U, contest_team_members AS CTM 
 WHERE CTM.team_id = T.id AND U.id = CTM.customuser_id AND T.onsite = 1 ;
@@ -79,9 +102,11 @@ EXECUTE stmt;
     MYSQL_CON.commit()
     MYSQL_CON.close()
 
+    print "wrote csv to /tmp/csv_teamuser"
+
 if __name__ == "__main__":
-    #process_team_contestants()
-    extract_to_csv()
+    process_team_contestants()
+    #extract_to_csv()
 
 
 # EOF
